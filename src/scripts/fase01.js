5f320eef0e77053db53cce1f234a892fb6784235
Vinícius Abdon <viabdon04@gmail.com>
2023-04-29 17:21:48 -0300

    Maior eficiencia na escrita da matriz
    utilização de if else

--- src/scripts/fase01.js
+++ src/scripts/fase01.js
@@ -4,295 +4,120 @@ for (let i = 0; i < 17; i++)
 {
     fase01[i] = new Array(17);
 };
+
 for (let i = 0; i < 17; i++) 
 {
     for(let j = 0; j < 17; j++)
     {
         switch (i) {
             case 1:
-                fase01[i][0] = '*'
-                fase01[i][1] = ' '
-                fase01[i][2] = ' '
-                fase01[i][3] = ' '
-                fase01[i][4] = ' '
-                fase01[i][5] = ' '
-                fase01[i][6] = ' '
-                fase01[i][7] = ' '
-                fase01[i][8] = ' '
-                fase01[i][9] = ' '
-                fase01[i][10] = ' '
-                fase01[i][11] = ' '
-                fase01[i][12] = ' '
-                fase01[i][13] = ' '
-                fase01[i][14] = ' '
-                fase01[i][15] = ' '
-                fase01[i][16] = '*'
+                if (j === 0 || j === 16) {
+                    fase01[i][j] = '*'
+                } else {
+                    fase01[i][j] = ' '
+                }
             break;
             case 2:
-                fase01[i][0] = '*'
-                fase01[i][1] = ' '
-                fase01[i][2] = '&'
-                fase01[i][3] = ' '
-                fase01[i][4] = ' '
-                fase01[i][5] = ' '
-                fase01[i][6] = ' '
-                fase01[i][7] = ' '
-                fase01[i][8] = ' '
-                fase01[i][9] = ' '
-                fase01[i][10] = ' '
-                fase01[i][11] = ' '
-                fase01[i][12] = ' '
-                fase01[i][13] = ' '
-                fase01[i][14] = ' '
-                fase01[i][15] = ' '
-                fase01[i][16] = 'D'
+                if (j === 0) {
+                    fase01[i][j] = '*'
+                } else if (j === 2) {
+                    fase01[i][j] = ' '
+                } else {
+                    fase01[i][j] = ' '
+                }
             break;
             case 3:
-                fase01[i][0] = '*'
-                fase01[i][1] = ' '
-                fase01[i][2] = ' '
-                fase01[i][3] = ' '
-                fase01[i][4] = ' '
-                fase01[i][5] = ' '
-                fase01[i][6] = ' '
-                fase01[i][7] = ' '
-                fase01[i][8] = ' '
-                fase01[i][9] = ' '
-                fase01[i][10] = ' '
-                fase01[i][11] = ' '
-                fase01[i][12] = ' '
-                fase01[i][13] = ' '
-                fase01[i][14] = ' '
-                fase01[i][15] = ' '
-                fase01[i][16] = '*'
+                if (j === 0 || j === 16) {
+                    fase01[i][j] = '*'
+                } else {
+                    fase01[i][j] = ' '
+                }
             break;
             case 4:
-                fase01[i][0] = '*'
-                fase01[i][1] = '*'
-                fase01[i][2] = '*'
-                fase01[i][3] = '*'
-                fase01[i][4] = '*'
-                fase01[i][5] = '*'
-                fase01[i][6] = '*'
-                fase01[i][7] = '*'
-                fase01[i][8] = '*'
-                fase01[i][9] = '*'
-                fase01[i][10] = '*'
-                fase01[i][11] = ' '
-                fase01[i][12] = ' '
-                fase01[i][13] = ' '
-                fase01[i][14] = ' '
-                fase01[i][15] = ' '
-                fase01[i][16] = '*'
+                if (j >= 11 && j <= 15) {
+                    fase01[i][j] = ' '
+                } else {
+                    fase01[i][j] = '*'
+                }
             break;
             case 5:
-                fase01[i][0] = '*'
-                fase01[i][1] = ' '
-                fase01[i][2] = ' '
-                fase01[i][3] = ' '
-                fase01[i][4] = ' '
-                fase01[i][5] = ' '
-                fase01[i][6] = ' '
-                fase01[i][7] = ' '
-                fase01[i][8] = ' '
-                fase01[i][9] = ' '
-                fase01[i][10] = ' '
-                fase01[i][11] = ' '
-                fase01[i][12] = ' '
-                fase01[i][13] = ' '
-                fase01[i][14] = ' '
-                fase01[i][15] = ' '
-                fase01[i][16] = '*'
+                if (j === 0 || j === 16) {
+                    fase01[i][j] = '*'
+                } else {
+                    fase01[i][j] = ' '
+                }
             break;
             case 6:
-                fase01[i][0] = '*'
-                fase01[i][1] = ' '
-                fase01[i][2] = ' '
-                fase01[i][3] = ' '
-                fase01[i][4] = ' '
-                fase01[i][5] = ' '
-                fase01[i][6] = ' '
-                fase01[i][7] = ' '
-                fase01[i][8] = ' '
-                fase01[i][9] = ' '
-                fase01[i][10] = ' '
-                fase01[i][11] = ' '
-                fase01[i][12] = ' '
-                fase01[i][13] = ' '
-                fase01[i][14] = ' '
-                fase01[i][15] = ' '
-                fase01[i][16] = '*'
+                if (j === 0 || j === 16) {
+                    fase01[i][j] = '*'
+                } else {
+                    fase01[i][j] = ' '
+                }
             break;
             case 7:
-                fase01[i][0] = '*'
-                fase01[i][1] = ' '
-                fase01[i][2] = ' '
-                fase01[i][3] = ' '
-                fase01[i][4] = ' '
-                fase01[i][5] = ' '
-                fase01[i][6] = ' '
-                fase01[i][7] = ' '
-                fase01[i][8] = ' '
-                fase01[i][9] = ' '
-                fase01[i][10] = ' '
-                fase01[i][11] = ' '
-                fase01[i][12] = ' '
-                fase01[i][13] = ' '
-                fase01[i][14] = ' '
-                fase01[i][15] = ' '
-                fase01[i][16] = '*'
+                if (j === 0 || j === 16) {
+                    fase01[i][j] = '*'
+                } else {
+                    fase01[i][j] = ' '
+                }
             break;
             case 8:
-                fase01[i][0] = '*'
-                fase01[i][1] = ' '
-                fase01[i][2] = ' '
-                fase01[i][3] = ' '
-                fase01[i][4] = ' '
-                fase01[i][5] = ' '
-                fase01[i][6] = '*'
-                fase01[i][7] = '*'
-                fase01[i][8] = '*'
-                fase01[i][9] = '*'
-                fase01[i][10] = '*'
-                fase01[i][11] = '*'
-                fase01[i][12] = '*'
-                fase01[i][13] = '*'
-                fase01[i][14] = '*'
-                fase01[i][15] = '*'
-                fase01[i][16] = '*'
+                if (j >= 1 && j <= 5) {
+                    fase01[i][j] = ' '
+                } else {
+                    fase01[i][j] = '*'
+                }
             break;
             case 9:
-                fase01[i][0] = '*'
-                fase01[i][1] = ' '
-                fase01[i][2] = ' '
-                fase01[i][3] = ' '
-                fase01[i][4] = ' '
-                fase01[i][5] = ' '
-                fase01[i][6] = ' '
-                fase01[i][7] = ' '
-                fase01[i][8] = ' '
-                fase01[i][9] = ' '
-                fase01[i][10] = ' '
-                fase01[i][11] = ' '
-                fase01[i][12] = ' '
-                fase01[i][13] = 
-                fase01[i][14] = 
-                fase01[i][15] = 
-                fase01[i][16] = '*'
+                if (j === 0 || j === 16) {
+                    fase01[i][j] = '*'
+                } else {
+                    fase01[i][j] = ' '
+                }
             break;
             case 10:
-                fase01[i][0] = '*'
-                fase01[i][1] = ' '
-                fase01[i][2] = ' '
-                fase01[i][3] = ' '
-                fase01[i][4] = ' '
-                fase01[i][5] = ' '
-                fase01[i][6] = ' '
-                fase01[i][7] = ' '
-                fase01[i][8] = ' '
-                fase01[i][9] = ' '
-                fase01[i][10] = ' '
-                fase01[i][11] = ' '
-                fase01[i][12] = ' '
-                fase01[i][13] = ' '
-                fase01[i][14] = ' '
-                fase01[i][15] = ' '
-                fase01[i][16] = '*'
+                if (j === 0 || j === 16) {
+                    fase01[i][j] = '*'
+                } else {
+                    fase01[i][j] = ' '
+                }
             break;
             case 11:
-                fase01[i][0] = '*'
-                fase01[i][1] = ' '
-                fase01[i][2] = ' '
-                fase01[i][3] = ' '
-                fase01[i][4] = ' '
-                fase01[i][5] = ' '
-                fase01[i][6] = ' '
-                fase01[i][7] = ' '
-                fase01[i][8] = ' '
-                fase01[i][9] = ' '
-                fase01[i][10] = ' '
-                fase01[i][11] = ' '
-                fase01[i][12] = ' '
-                fase01[i][13] = ' '
-                fase01[i][14] = ' '
-                fase01[i][15] = ' '
-                fase01[i][16] = '*'
+                if (j === 0 || j === 16) {
+                    fase01[i][j] = '*'
+                } else {
+                    fase01[i][j] = ' '
+                }
             break;
             case 12:
-                fase01[i][0] = '*'
-                fase01[i][1] = '*'
-                fase01[i][2] = '*'
-                fase01[i][3] = '*'
-                fase01[i][4] = '*'
-                fase01[i][5] = '*'
-                fase01[i][6] = '*'
-                fase01[i][7] = '*'
-                fase01[i][8] = '*'
-                fase01[i][9] = '*'
-                fase01[i][10] = '*'
-                fase01[i][11] = ' '
-                fase01[i][12] = ' '
-                fase01[i][13] = ' '
-                fase01[i][14] = ' '
-                fase01[i][15] = ' '
-                fase01[i][16] = '*'
+                if (j >= 11 && j <= 15) {
+                    fase01[i][j] = ' '
+                } else {
+                    fase01[i][j] = '*'
+                }
             break;
             case 13:
-                fase01[i][0] = '*'
-                fase01[i][1] = ' '
-                fase01[i][2] = ' '
-                fase01[i][3] = ' '
-                fase01[i][4] = '*'
-                fase01[i][5] = ' '
-                fase01[i][6] = ' '
-                fase01[i][7] = ' '
-                fase01[i][8] = ' '
-                fase01[i][9] = ' '
-                fase01[i][10] = '*'
-                fase01[i][11] = ' '
-                fase01[i][12] = ' '
-                fase01[i][13] = ' '
-                fase01[i][14] = ' '
-                fase01[i][15] = ' '
-                fase01[i][16] = '*'
+                if (j === 0 || j === 4 || j === 10 || j === 16) {
+                    fase01[i][j] = '*'
+                } else {
+                    fase01[i][j] = ' '
+                }
             break;
             case 14:
-                fase01[i][0] = '*'
-                fase01[i][1] = ' '
-                fase01[i][2] = 'o'
-                fase01[i][3] = ' '
-                fase01[i][4] = '*'
-                fase01[i][5] = ' '
-                fase01[i][6] = ' '
-                fase01[i][7] = '*'
-                fase01[i][8] = ' '
-                fase01[i][9] = ' '
-                fase01[i][10] = '*'
-                fase01[i][11] = ' '
-                fase01[i][12] = ' '
-                fase01[i][13] = ' '
-                fase01[i][14] = ' '
-                fase01[i][15] = ' '
-                fase01[i][16] = '*'
+                if (j === 0 || j === 4 || j === 7 || j === 10 || j === 16) {
+                    fase01[i][j] = '*'
+                } else if (j === 2) {
+                    fase01[i][j] = 'o'
+                } else {
+                    fase01[i][j] = ' '
+                }
             break;
             case 15:
-                fase01[i][0] = '*'
-                fase01[i][1] = ' '
-                fase01[i][2] = ' '
-                fase01[i][3] = ' '
-                fase01[i][4] = ' '
-                fase01[i][5] = ' '
-                fase01[i][6] = ' '
-                fase01[i][7] = '*'
-                fase01[i][8] = ' '
-                fase01[i][9] = ' '
-                fase01[i][10] = ' '
-                fase01[i][11] = ' '
-                fase01[i][12] = ' '
-                fase01[i][13] = ' '
-                fase01[i][14] = ' '
-                fase01[i][15] = ' '
-                fase01[i][16] = '*'
+                if (j === 0 || j === 7 || j === 16) {
+                    fase01[i][j] = '*'
+                } else {
+                    fase01[i][j] = ' '
+                }
             break;
             default: fase01[i][j] = "*"
             break;
