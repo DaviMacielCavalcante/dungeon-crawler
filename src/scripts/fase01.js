cd7e7023f80011af85a667bc7b4ce186a01f9d8c
Vinícius Abdon <viabdon04@gmail.com>
2023-04-30 15:22:07 -0300

    Movimentação do Player
    Tirei a function mapa também pois não era necessária e a partir da lógica estabelecida anteriormente criei um evento para a movimentação

--- src/scripts/fase01.js
+++ src/scripts/fase01.js
@@ -1,216 +1,170 @@
 let fase01 = [];
 
-let player = '&';
-let finalizado = 0;
-let direcao;
+const player = '&';
+let coordPlayer = {
+    x: 2,
+    y: 2,
+} ;
 
-function mapa() {
-    while (finalizado !== 1) {
-        for (let i = 0; i < 17; i++) {
-            fase01[i] = new Array(17);
-        };
+for (let i = 0; i < 17; i++) {
+    fase01[i] = new Array(17);
+};
     
-        for (let i = 0; i < 17; i++) {
-            for(let j = 0; j < 17; j++) {
-                switch (i) {
-                    case 1:
-                        if (j === 0 || j === 16) {
-                            fase01[i][j] = '*';
-                        } else {
-                            fase01[i][j] = ' ';
-                        }
-                    break;
-                    case 2:
-                        if (j === 0) {
-                            fase01[i][j] = '*';
-                        } else if (j === 2) {
-                            fase01[i][j] = player;
-                        } else if (j === 16) {
-                            fase01[i][j] = 'D';
-                        } else {
-                            fase01[i][j] = ' ';
-                        }
-                        break;
-                    case 3:
-                        if (j === 0 || j === 16) {
-                            fase01[i][j] = '*';
-                        } else {
-                            fase01[i][j] = ' ';
-                        }
-                        break;
-                    case 4:
-                        if (j >= 11 && j <= 15) {
-                            fase01[i][j] = ' ';
-                        } else {
-                            fase01[i][j] = '*';
-                        }
-                    break;
-                    case 5:
-                        if (j === 0 || j === 16) {
-                            fase01[i][j] = '*';
-                        } else {
-                            fase01[i][j] = ' ';
-                        }
-                        break;
-                    case 6:
-                        if (j === 0 || j === 16) {
-                            fase01[i][j] = '*';
-                        } else {
-                            fase01[i][j] = ' ';
-                        }
-                        break;
-                    case 7:
-                        if (j === 0 || j === 16) {
-                            fase01[i][j] = '*'
-                        } else {
-                            fase01[i][j] = ' '
-                        }
-                        break;
-                    case 8:
-                        if (j >= 1 && j <= 5) {
-                            fase01[i][j] = ' '
-                        } else {
-                            fase01[i][j] = '*'
-                        }
-                        break;
-                    case 9:
-                        if (j === 0 || j === 16) {
-                            fase01[i][j] = '*'
-                        } else {
-                            fase01[i][j] = ' '
-                        }
-                        break;
-                    case 10:
-                        if (j === 0 || j === 16) {
-                            fase01[i][j] = '*'
-                        } else {
-                            fase01[i][j] = ' '
-                        }
-                        break;
-                    case 11:
-                        if (j === 0 || j === 16) {
-                            fase01[i][j] = '*'
-                        } else {
-                            fase01[i][j] = ' '
-                        }
-                        break;
-                    case 12:
-                        if (j >= 11 && j <= 15) {
-                            fase01[i][j] = ' '
-                        } else {
-                            fase01[i][j] = '*'
-                        }
-                    break;
-                    case 13:
-                        if (j === 0 || j === 4 || j === 10 || j === 16) {
-                            fase01[i][j] = '*'
-                        } else {
-                            fase01[i][j] = ' '
-                        }
-                    break;
-                    case 14:
-                        if (j === 0 || j === 4 || j === 7 || j === 10 || j === 16) {
-                            fase01[i][j] = '*'
-                        } else if (j === 2) {
-                            fase01[i][j] = '@'
-                        } else {
-                            fase01[i][j] = ' '
-                        }
-                    break;
-                    case 15:
-                        if (j === 0 || j === 7 || j === 16) {
-                            fase01[i][j] = '*'
-                        } else {
-                            fase01[i][j] = ' '
-                        }
-                    break;
-                    default: fase01[i][j] = "*"
-                    break;
+for (let i = 0; i < 17; i++) {
+    for(let j = 0; j < 17; j++) {
+        switch (i) {
+            case 1:
+                if (j === 0 || j === 16) {
+                    fase01[i][j] = '*';
+                } else {
+                    fase01[i][j] = ' ';
                 }
-            }
-        };    
-        console.log(fase01);
-    };
-}
-
-
-
-function movement(direcao) {
-    switch (direcao) {
-        case 'w':
-            if (fase01[i][j] === '*' || fase01[i][j] === '#') {
-                alert('No');
-            } else if (fase01[i > 16][j] || fase01[i][j > 16] || fase01[i > 16][j > 16]){
-                alert('No');
-            } else if (fase01[i < 0][j] || fase01[i][j < 0] || fase01[i < 0][j < 0]){
-                alert('No');
-            } else {
-                for (let i = 0; i < 17; i ++) {
-                    for(let j = 0; j < 17; j ++) {
-                        if (fase01[i][j] === player) {
-                            fase01[i][j] = ' ';
-                            fase[i - 1][j] = player;
-                        }
-                    }
+            break;
+            case 2:
+                if (j === 0) {
+                    fase01[i][j] = '*';
+                } else if (j === 2) {
+                    fase01[i][j] = player;
+                } else if (j === 16) {
+                    fase01[i][j] = 'D';
+                } else {
+                    fase01[i][j] = ' ';
                 }
-            }
-            break;
-        case 's':
-            if (fase01[i][j] === '*' || fase01[i][j] === '#') {
-                alert('No');
-            } else if (fase01[i > 16][j] || fase01[i][j > 16] || fase01[i > 16][j > 16]){
-                alert('No');
-            } else if (fase01[i < 0][j] || fase01[i][j < 0] || fase01[i < 0][j < 0]){
-                alert('No');
-            } else {
-                for (let i = 0; i < 17; i ++) {
-                    for(let j = 0; j < 17; j ++) {
-                        if (fase01[i][j] === player) {
-                            fase01[i][j] = ' ';
-                            fase[i + 1][j] = player;
-                        }
-                    }
+                break;
+            case 3:
+                if (j === 0 || j === 16) {
+                    fase01[i][j] = '*';
+                } else {
+                    fase01[i][j] = ' ';
                 }
-            }
-            break;
-        case 'a':
-            if (fase01[i][j] === '*' || fase01[i][j] === '#') {
-                alert('No');
-            } else if (fase01[i > 16][j] || fase01[i][j > 16] || fase01[i > 16][j > 16]){
-                alert('No');
-            } else if (fase01[i < 0][j] || fase01[i][j < 0] || fase01[i < 0][j < 0]){
-                alert('No');
-            } else {
-                for (let i = 0; i < 17; i ++) {
-                    for(let j = 0; j < 17; j ++) {
-                        if (fase01[i][j] === player) {
-                            fase01[i][j] = ' ';
-                            fase[i][j - 1] = player;
-                        }
-                    }
+                break;
+            case 4:
+                if (j >= 11 && j <= 15) {
+                    fase01[i][j] = ' ';
+                } else {
+                    fase01[i][j] = '*';
                 }
-            }
             break;
-        case 'd':
-            if (fase01[i][j] === '*' || fase01[i][j] === '#') {
-                alert('No');
-            } else if (fase01[i > 16][j] || fase01[i][j > 16] || fase01[i > 16][j > 16]){
-                alert('No');
-            } else if (fase01[i < 0][j] || fase01[i][j < 0] || fase01[i < 0][j < 0]){
-                alert('No');
-            } else {
-                for (let i = 0; i < 17; i ++) {
-                    for(let j = 0; j < 17; j ++) {
-                        if (fase01[i][j] === player) {
-                            fase01[i][j] = ' ';
-                            fase[i][j + 1] = player;
-                        }
-                    }
+            case 5:
+                if (j === 0 || j === 16) {
+                    fase01[i][j] = '*';
+                } else {
+                    fase01[i][j] = ' ';
                 }
-            }
+                break;
+            case 6:
+                if (j === 0 || j === 16) {
+                    fase01[i][j] = '*';
+                } else {
+                    fase01[i][j] = ' ';
+                }
+                break;
+            case 7:
+                if (j === 0 || j === 16) {
+                    fase01[i][j] = '*'
+                } else {
+                    fase01[i][j] = ' '
+                }
+                break;
+            case 8:
+                if (j >= 1 && j <= 5) {
+                    fase01[i][j] = ' '
+                } else {
+                    fase01[i][j] = '*'
+                }
+                break;
+            case 9:
+                if (j === 0 || j === 16) {
+                    fase01[i][j] = '*'
+                } else {
+                    fase01[i][j] = ' '
+                }
+                break;
+            case 10:
+                if (j === 0 || j === 16) {
+                    fase01[i][j] = '*'
+                } else {
+                    fase01[i][j] = ' '
+                }
+                break;
+            case 11:
+                if (j === 0 || j === 16) {
+                    fase01[i][j] = '*'
+                } else {
+                    fase01[i][j] = ' '
+                }
+                break;
+            case 12:
+                if (j >= 11 && j <= 15) {
+                    fase01[i][j] = ' '
+                } else {
+                    fase01[i][j] = '*'
+                }
             break;
-        default:
-            alert('Movimentação Inválida');
+            case 13:
+                if (j === 0 || j === 4 || j === 10 || j === 16) {
+                    fase01[i][j] = '*'
+                } else {
+                    fase01[i][j] = ' '
+                }
             break;
+            case 14:
+                if (j === 0 || j === 4 || j === 7 || j === 10 || j === 16) {
+                    fase01[i][j] = '*'
+                } else if (j === 2) {
+                    fase01[i][j] = '@'
+                } else {
+                    fase01[i][j] = ' '
+                }
+            break;
+            case 15:
+                if (j === 0 || j === 7 || j === 16) {
+                    fase01[i][j] = '*'
+                } else {
+                    fase01[i][j] = ' '
+                }
+            break;
+            default: fase01[i][j] = "*"
+            break;
+        }
     }
-}
+};    
+console.log(fase01);
+
+
+document.addEventListener ('keydown', (event) => {
+    switch (event.keyCode) {
+        case 87: // W
+            if (fase01[coordPlayer.y - 1][coordPlayer.x] === ' ') {
+                fase01[coordPlayer.y][coordPlayer.x] = ' ';
+                fase01[coordPlayer.y - 1][coordPlayer.x] = '&';
+                coordPlayer.y--;
+                console.log(fase01);
+            } 
+            break;
+            case 83: // S
+                if (fase01[coordPlayer.y + 1][coordPlayer.x] === ' ') {
+                    fase01[coordPlayer.y][coordPlayer.x] = ' ';
+                    fase01[coordPlayer.y + 1][coordPlayer.x] = '&';
+                    coordPlayer.y++;
+                    console.log(fase01);
+                }
+                break;
+            case 65: // A
+                if (fase01[coordPlayer.y][coordPlayer.x - 1] === ' ') {
+                    fase01[coordPlayer.y][coordPlayer.x] = ' ';
+                    fase01[coordPlayer.y][coordPlayer.x - 1] = '&';
+                    coordPlayer.x--;
+                    console.log(fase01);
+                }
+            break;
+            case 68: // D
+                if(fase01[coordPlayer.y][coordPlayer.x + 1] === ' ') {
+                    fase01[coordPlayer.y][coordPlayer.x] = ' ';
+                    fase01[coordPlayer.y][coordPlayer.x + 1] = '&';
+                    coordPlayer.x++;
+                    console.log(fase01);
+                }
+            break;
+    };
+});
