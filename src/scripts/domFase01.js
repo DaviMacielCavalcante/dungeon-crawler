ad05fedcb6c515212785022a6ebc7a5fda1205f2
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2023-05-06 12:16:22 -0300

    Implementaçao do DOM da fase 01 e inicio da 02

--- src/scripts/domFase01.js
+++ src/scripts/domFase01.js
@@ -22,10 +22,11 @@ function mapa() {
         for(let j = 0; j < 15; j++) {
             switch (i) {
                 case 1:
+                    if (coordPlayer.x === i && coordPlayer.y === j) {                        
+                        fase01[i][j] = player;
+                    }
                     if (j === 0) {
                         fase01[i][j] = '*';
-                    } else if (j === 1) {
-                        fase01[i][j] = player;
                     } else if (j === 14) {
                         fase01[i][j] = lockedDoor;
                     } else {
@@ -35,6 +36,8 @@ function mapa() {
                 case 2:
                     if (j === 0 || j === 14) {
                         fase01[i][j] = '*';
+                    } else if (coordPlayer.x === i && coordPlayer.y === j) {                        
+                        fase01[i][j] = player;
                     } else {
                         fase01[i][j] = ' ';
                     }
@@ -49,6 +52,8 @@ function mapa() {
                 case 4:
                     if (j === 0 || j === 14) {
                         fase01[i][j] = '*';
+                    }  else if (coordPlayer.x === i && coordPlayer.y === j) {                        
+                        fase01[i][j] = player;
                     } else {
                         fase01[i][j] = ' ';
                     }
@@ -56,6 +61,8 @@ function mapa() {
                 case 5:
                     if (j === 0 || j === 14) {
                         fase01[i][j] = '*';
+                    }  else if (coordPlayer.x === i && coordPlayer.y === j) {                        
+                        fase01[i][j] = player;
                     } else {
                         fase01[i][j] = ' ';
                     }
@@ -63,6 +70,8 @@ function mapa() {
                 case 6:
                     if (j >= 1 && j <= 3) {
                         fase01[i][j] = ' '
+                    }  else if (coordPlayer.x === i && coordPlayer.y === j) {                        
+                        fase01[i][j] = player;
                     } else {
                         fase01[i][j] = '*'
                     }
@@ -70,6 +79,8 @@ function mapa() {
                 case 7:
                     if (j === 0 || j === 14) {
                         fase01[i][j] = '*'
+                    }  else if (coordPlayer.x === i && coordPlayer.y === j) {                        
+                        fase01[i][j] = player;
                     } else {
                         fase01[i][j] = ' '
                     }
@@ -77,14 +88,17 @@ function mapa() {
                 case 8:
                     if (j === 0 || j === 14) {
                         fase01[i][j] = '*';
+                    } else if (coordPlayer.x === i && coordPlayer.y === j) {                        
+                        fase01[i][j] = player;
                     } else {
                         fase01[i][j] = ' ';
                     }
                     break;
-                    break;
                 case 9:
                     if (j >= 11 && j <= 13) {
                         fase01[i][j] = ' ';
+                    } else if (coordPlayer.x === i && coordPlayer.y === j) {                        
+                        fase01[i][j] = player;
                     } else {
                         fase01[i][j] = '*';
                     }
@@ -92,6 +106,8 @@ function mapa() {
                 case 10:
                     if (j === 0 || j === 4 || j === 8 || j === 14) {
                         fase01[i][j] = '*'
+                    } else if (coordPlayer.x === i && coordPlayer.y === j) {                        
+                        fase01[i][j] = player;
                     } else {
                         fase01[i][j] = ' '
                     }
@@ -99,6 +115,8 @@ function mapa() {
                 case 11:
                     if (j === 0 || j === 4 || j === 6 || j === 8 || j === 10 || j === 14) {
                         fase01[i][j] = '*'
+                    } else if (coordPlayer.x === i && coordPlayer.y === j) {                        
+                        fase01[i][j] = player;
                     } else {
                         fase01[i][j] = ' '
                     }
@@ -108,6 +126,8 @@ function mapa() {
                         fase01[i][j] = '*'
                     } else if (j === 2) {
                         fase01[i][j] = key;
+                    } else if (coordPlayer.x === i && coordPlayer.y === j) {                        
+                        fase01[i][j] = player;
                     } else {
                         fase01[i][j] = ' '
                     }
@@ -115,6 +135,8 @@ function mapa() {
                 case 13:
                     if (j === 0 || j === 6 || j === 10 || j === 14) {
                         fase01[i][j] = '*'
+                    } else if (coordPlayer.x === i && coordPlayer.y === j) {                        
+                        fase01[i][j] = player;
                     } else {
                         fase01[i][j] = ' '
                     }
@@ -126,12 +148,63 @@ function mapa() {
     }; 
 }
 
-function carregarMapa() {
-    mapa();
+function checarEspacoVazio(direcao) {
+    switch (direcao) {
+        case 87:
+            return fase01[coordPlayer.y - 1][coordPlayer.x] === ' ';
+        case 83:
+            return fase01[coordPlayer.y + 1][coordPlayer.x] === ' ';
+        case 65:
+            return fase01[coordPlayer.y][coordPlayer.x - 1] === ' ';
+        case 68:
+            return fase01[coordPlayer.y][coordPlayer.x + 1] === ' ';
+    }
+};
+
+function checarEspacoChave(direcao) {
+    switch (direcao) {
+        case 87:
+            return fase01[coordPlayer.y - 1][coordPlayer.x] === key;
+        case 83:
+            return fase01[coordPlayer.y + 1][coordPlayer.x] === key;
+        case 65:
+            return fase01[coordPlayer.y][coordPlayer.x - 1] === key;
+        case 68:
+            return fase01[coordPlayer.y][coordPlayer.x + 1] === key;
+    }
+};
+
+function checarPorta(direcao) {
+    switch (direcao) {
+        case 87:
+            return fase01[coordPlayer.y - 1][coordPlayer.x] === unlockedDoor;
+        case 83:
+            return fase01[coordPlayer.y + 1][coordPlayer.x] === unlockedDoor;
+        case 65:
+            return fase01[coordPlayer.y][coordPlayer.x - 1] === unlockedDoor;
+        case 68:
+            return fase01[coordPlayer.y][coordPlayer.x + 1] === unlockedDoor;
+    }
+};
+
+function estaNaChaveEPortaTrancada() {
+    return coordPlayer.x === coordKey.x && coordPlayer.y === coordKey.y && fase01[coordDoor.y][coordDoor.x] === lockedDoor;
+};
+function acimaDaChave() {
+    return coordPlayer.x === coordKey.x && coordPlayer.y === coordKey.y;
+};
+function acimaDaPortaDestrancada() {
+    return coordPlayer.x === coordDoor.x && coordPlayer.y === coordDoor.y;
+};
+
+function carregarMapa() {    
     const tela = document.querySelector('#tela-jogo');
     let linha = [];
     let coluna;
 
+    tela.innerHTML = '';
+
+    
     for (let i = 0; i < 15; i++) {
         linha = document.createElement('tr');
         for (let j = 0; j < 15; j++) {            
@@ -142,7 +215,94 @@ function carregarMapa() {
             linha.appendChild(coluna);
         };
         tela.appendChild(linha);
-    };    
+    };  
+    
 };
 
+document.addEventListener ('keydown', (controles) => {
+    switch (controles.keyCode) {
+            case 87: // W
+                if (checarEspacoVazio(87) || checarEspacoChave(87) || checarPorta(87)) {
+
+                    if (coordPlayer.x === coordKey.x && coordPlayer.y === coordKey.y && fase01[coordDoor.y][coordDoor.x] === lockedDoor) {
+                        fase01[coordPlayer.y][coordPlayer.x] = key;
+                        fase01[coordPlayer.y - 1][coordPlayer.x] = player;
+                        coordPlayer.y--;
+                        carregarMapa();
+
+                    } else {
+                        fase01[coordPlayer.y][coordPlayer.x] = ' ';
+                        fase01[coordPlayer.y - 1][coordPlayer.x] = player;
+                        coordPlayer.y--;
+                        carregarMapa();
+
+                    }
+                }
+                break;
+            case 83: // S
+                if (checarEspacoVazio(83)|| checarEspacoChave(83) || checarPorta(83)) {
+
+                    if (coordPlayer.x === coordKey.x && coordPlayer.y === coordKey.y && fase01[coordDoor.y][coordDoor.x] === lockedDoor) {
+                        fase01[coordPlayer.y][coordPlayer.x] = key;
+                        fase01[coordPlayer.y + 1][coordPlayer.x] = player;
+                        coordPlayer.y++;
+                        carregarMapa();
+
+                    } else {
+                        fase01[coordPlayer.y][coordPlayer.x] = ' ';
+                        fase01[coordPlayer.y + 1][coordPlayer.x] = player;
+                        coordPlayer.y++;
+                        carregarMapa();
+
+                    }
+                }
+                break;
+            case 65: // A
+                if (checarEspacoVazio(65) || checarEspacoChave(65) || checarPorta(65)) {
+
+                    if (coordPlayer.x === coordKey.x && coordPlayer.y === coordKey.y && fase01[coordDoor.y][coordDoor.x] === lockedDoor) {
+                        fase01[coordPlayer.y][coordPlayer.x] = key;
+                        fase01[coordPlayer.y][coordPlayer.x - 1] = player;
+                        coordPlayer.x--;
+                        carregarMapa();
+
+                    } else {
+                        fase01[coordPlayer.y][coordPlayer.x] = ' ';
+                        fase01[coordPlayer.y][coordPlayer.x - 1] = player;
+                        coordPlayer.x--;
+                        carregarMapa();
+                    }
+                }
+                break;
+            case 68: // D
+                if (checarEspacoVazio(68) || checarEspacoChave(68) || checarPorta(68)) {
+
+                    if (coordPlayer.x === coordKey.x && coordPlayer.y === coordKey.y && fase01[coordDoor.y][coordDoor.x] === lockedDoor) {
+                        fase01[coordPlayer.y][coordPlayer.x] = key;
+                        fase01[coordPlayer.y][coordPlayer.x + 1] = player;
+                        coordPlayer.x++;                        
+                        carregarMapa();
+
+                    } else {
+                        fase01[coordPlayer.y][coordPlayer.x] = ' ';
+                        fase01[coordPlayer.y][coordPlayer.x + 1] = player;
+                        coordPlayer.x++;
+                        
+                        carregarMapa();
+
+                    }
+                }
+                break;
+            case 73: // I
+                if (acimaDaChave()) {
+                    fase01[coordDoor.y][coordDoor.x] = unlockedDoor;
+                    carregarMapa(); 
+                }
+                if (acimaDaPortaDestrancada()) {
+                    alert('proxima fase')
+                }
+    };
+});
+
+document.addEventListener("DOMContentLoaded", mapa);
 document.addEventListener("DOMContentLoaded", carregarMapa);
