248809e242e48ba7a235a02e8e7749ee36e8094c
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2023-05-08 23:56:42 -0300

    Novas interações quase finalizadas
    Interação com espinhos, botão e o surgimento de espinhos no mapa e destrancamento de portas está feito. Faltam apenas dois botões. Correção no tutorial sobre portas destrancadas.

--- src/scripts/fase02.js
+++ src/scripts/fase02.js
@@ -1,12 +1,14 @@
 let fase02 = [];
-let redDoor, purpleDoor, pinkDoor, fakeWall;
-const player = '&', spikes = '#', key = '@', lockedDoor = 'D', unlockedDoor = '=';
+let redDoor, purpleDoor, pinkDoor;
+const player = '&', spikes = '#', key = '@', lockedDoor = 'D', unlockedDoor = '=', button = 'O';
 const redKey = key, purpleKey = key, pinkKey = key;
-const  greenButton = 'O', darkBlueButton = 'O', lightBlueButton = 'O', yellowButton = 'O', orangeButton = 'O';
-let coordPlayer = {x: 2, y: 2,};
+const  greenButton = button, darkBlueButton = button, lightBlueButton = button, yellowButton = button, orangeButton = button;
+let coordPlayer = {x: 15, y: 13,};
 const fixedCoords = {
+    redDoor: {x: 9, y: 16},
+    purpleDoor: {x: 15, y: 11},
     finalDoor: {x: 29, y: 14},
-    redKey: {x: 10, y: 12},
+    redKey: {x: 10, y: 11},
     purpleKey: {x: 20, y: 27},
     pinkKey: {x: 28, y: 11},
     greenButton: {x: 2, y: 14},
@@ -34,8 +36,6 @@ for (let i = 0; i < 30; i++) {
             case 2:
                 if (j === 0 || j === 12 || j === 22 || j === 26 || j === 29) {
                     fase02[i][j] = '*';
-                } else if (j === coordPlayer.x) {
-                    fase02[i][j] = player;
                 } else {
                     fase02[i][j] = ' ';
                 }
@@ -233,10 +233,11 @@ for (let i = 0; i < 30; i++) {
                     fase02[i][j] = '*';
                 } else if (j === fixedCoords.purpleKey.x) {
                     fase02[i][j] =  purpleKey;
+                } else if (j === coordPlayer.x) {
+                    fase02[i][j] = player;
                 } else {
                     fase02[i][j] = ' ';
                 }
-                break;
                 break;
             case 28:
                 if (j === 0 || j === 11 || j === 15 || j === 22 || j === 29) {
@@ -292,6 +293,19 @@ function checarPorta(direcao) {
     }
 };
 
+function checarBotao(direcao) {
+    switch(direcao) {
+        case 87:
+            return fase02[coordPlayer.y - 1][coordPlayer.x] === button;
+        case 83:
+            return fase02[coordPlayer.y + 1][coordPlayer.x] === button;
+        case 65:
+            return fase02[coordPlayer.y][coordPlayer.x - 1] === button;
+        case 68:
+            return fase02[coordPlayer.y][coordPlayer.x + 1] === button;
+    }
+}
+
 function estaNaChaveVermelha() {
     return coordPlayer.x === fixedCoords.redKey.x && coordPlayer.y === fixedCoords.redKey.y;
 };
@@ -324,137 +338,331 @@ function estaNoBotaoLaranja() {
     return coordPlayer.x === fixedCoords.orangeButton.x && coordPlayer.y === fixedCoords.orangeButton.y;
 };
 
+function estaNumaPorta() {
+    if (coordPlayer.x === fixedCoords.redDoor.x && coordPlayer.y === fixedCoords.redDoor.y) {
+        return true
+    } else if (coordPlayer.x === fixedCoords.purpleDoor.x && coordPlayer.y === fixedCoords.purpleDoor.y) {
+        return true
+    }
+}
+
 function estaNaPortaFinal() {
-    return coordPlayer.x === coordDoor.x && coordPlayer.y === coordDoor.y;
+    return coordPlayer.x === fixedCoords.finalDoor.x && coordPlayer.y === fixedCoords.finalDoor.y;
 };
+
+function manterChaveIrParaCima() {
+    fase02[coordPlayer.y][coordPlayer.x] = key;
+    fase02[coordPlayer.y - 1][coordPlayer.x] = player;
+    coordPlayer.y--;
+    console.clear();
+    console.table(fase02);
+};
+
+function irParaCima() {
+    fase02[coordPlayer.y][coordPlayer.x] = ' ';
+    fase02[coordPlayer.y - 1][coordPlayer.x] = player;
+    coordPlayer.y--;
+    console.clear();
+    console.log(coordPlayer.y)
+    console.table(fase02);
+};
+
+function manterChaveIrParaBaixo() {
+    fase02[coordPlayer.y][coordPlayer.x] = key;
+    fase02[coordPlayer.y + 1][coordPlayer.x] = player;
+    coordPlayer.y++;
+    console.clear();
+    console.table(fase02);
+};
+
+function irParaBaixo() {
+    fase02[coordPlayer.y][coordPlayer.x] = ' ';
+    fase02[coordPlayer.y + 1][coordPlayer.x] = player;
+    coordPlayer.y++;
+    console.clear();
+    console.table(fase02);
+};
+
+function manterChaveIrParaEsquerda() {
+    fase02[coordPlayer.y][coordPlayer.x] = key;
+    fase02[coordPlayer.y][coordPlayer.x - 1] = player;
+    coordPlayer.x--;
+    console.clear();
+    console.table(fase02);
+};
+
+function irParaEsquerda() {
+    fase02[coordPlayer.y][coordPlayer.x] = ' ';
+    fase02[coordPlayer.y][coordPlayer.x - 1] = player;
+    coordPlayer.x--;
+    console.clear();
+    console.table(fase02);
+}
+
+function manterChaveIrParaDireita() {
+    fase02[coordPlayer.y][coordPlayer.x] = key;
+    fase02[coordPlayer.y][coordPlayer.x + 1] = player;
+    coordPlayer.x++;
+    console.clear();
+    console.table(fase02);
+}
+
+function irParaDireita() {
+    fase02[coordPlayer.y][coordPlayer.x] = ' ';
+    fase02[coordPlayer.y][coordPlayer.x + 1] = player;
+    coordPlayer.x++;
+    console.clear();
+    console.table(fase02);
+}
+function manterBotaoIrParaCima() {
+    fase02[coordPlayer.y][coordPlayer.x] = button;
+    fase02[coordPlayer.y - 1][coordPlayer.x] = player;
+    coordPlayer.y--;
+    console.clear();
+    console.log(coordPlayer.y)
+    console.table(fase02);
+}
+function manterBotaoIrParaBaixo() {
+    fase02[coordPlayer.y][coordPlayer.x] = button;
+    fase02[coordPlayer.y + 1][coordPlayer.x] = player;
+    coordPlayer.y++;
+    console.clear();
+    console.table(fase02);
+}
+function manterBotaoIrParaEsquerda() {
+    fase02[coordPlayer.y][coordPlayer.x] = button;
+    fase02[coordPlayer.y][coordPlayer.x - 1] = player;
+    coordPlayer.x--;
+    console.clear();
+    console.table(fase02);
+}
+function manterBotaoIrParaDireita() {
+    fase02[coordPlayer.y][coordPlayer.x] = button;
+    fase02[coordPlayer.y][coordPlayer.x + 1] = player;
+    coordPlayer.x++;
+    console.clear();
+    console.table(fase02);
+}
+function sairDoBotao() {
+    if (estaNoBotaoVerde() || estaNoBotaoAzulEscuro() || estaNoBotaoAzulClaro() || estaNoBotaoAmarelo() || estaNoBotaoLaranja()) {
+        return true;
+    }
+}
+function manterPortaIrParaCima() {
+    fase02[coordPlayer.y][coordPlayer.x] = unlockedDoor;
+    fase02[coordPlayer.y - 1][coordPlayer.x] = player;
+    coordPlayer.y--;
+    console.clear();
+    console.log(coordPlayer.y)
+    console.table(fase02);
+}
+function manterPortaIrParaBaixo() {
+    fase02[coordPlayer.y][coordPlayer.x] = unlockedDoor;
+    fase02[coordPlayer.y + 1][coordPlayer.x] = player;
+    coordPlayer.y++;
+    console.clear();
+    console.table(fase02);
+}
+function manterPortaIrParaEsquerda() {
+    fase02[coordPlayer.y][coordPlayer.x] = unlockedDoor;
+    fase02[coordPlayer.y][coordPlayer.x - 1] = player;
+    coordPlayer.x--;
+    console.clear();
+    console.table(fase02);
+}
+function manterPortaIrParaDireita() {
+    fase02[coordPlayer.y][coordPlayer.x] = unlockedDoor;
+    fase02[coordPlayer.y][coordPlayer.x + 1] = player;
+    coordPlayer.x++;
+    console.clear();
+    console.table(fase02);
+}
 
 document.addEventListener ('keydown', (controles) => {
     switch (controles.keyCode) {
             case 87: // W
-                if (checarEspacoVazio(87) || checarEspacoChave(87) || checarPorta(87)) {
+                if (checarEspacoVazio(87) || checarEspacoChave(87) || checarPorta(87) || checarBotao(87)) {
 
                     if (estaNaChaveVermelha() && redDoor === lockedDoor) {
-                        fase02[coordPlayer.y][coordPlayer.x] = redKey;
-                        fase02[coordPlayer.y - 1][coordPlayer.x] = player;
-                        coordPlayer.y--;
-                        console.clear();
-                        console.log(fase02);
+                        manterChaveIrParaCima();
 
                     } else if (estaNaChaveRoxa() && purpleDoor === lockedDoor) {
-                        fase02[coordPlayer.y][coordPlayer.x] = purpleKey;
-                        fase02[coordPlayer.y - 1][coordPlayer.x] = player;
-                        coordPlayer.y--;
-                        console.clear();
-                        console.log(fase02);
+                        manterChaveIrParaCima();
 
                     } else if (estaNaChaveRosa() && pinkDoor === lockedDoor) {
-                        fase02[coordPlayer.y][coordPlayer.x] = pinkKey;
-                        fase02[coordPlayer.y - 1][coordPlayer.x] = player;
-                        coordPlayer.y--;
-                        console.clear();
-                        console.log(fase02);
+                        manterChaveIrParaCima();
+
+                    } else if (sairDoBotao()) {
+                        manterBotaoIrParaCima();
+
+                    } else if (estaNumaPorta()) {
+                        manterPortaIrParaCima();
 
                     } else {
-                        fase02[coordPlayer.y][coordPlayer.x] = ' ';
-                        fase02[coordPlayer.y - 1][coordPlayer.x] = player;
-                        coordPlayer.y--;
-                        console.log(coordPlayer.y)
-                        console.clear();
-                        console.log(fase02);
-
+                        irParaCima();
                     }
                 }
                 break;
             case 83: // S
-                if (checarEspacoVazio(83)|| checarEspacoChave(83) || checarPorta(83)) {
+                if (checarEspacoVazio(83)|| checarEspacoChave(83) || checarPorta(83) || checarBotao(83)) {
 
                     if (estaNaChaveVermelha() && redDoor === lockedDoor) {
-                        fase02[coordPlayer.y][coordPlayer.x] = key;
-                        fase02[coordPlayer.y + 1][coordPlayer.x] = player;
-                        coordPlayer.y++;
-                        console.log(fase02);
+                        manterChaveIrParaBaixo();
+
+                    } else if (estaNaChaveRoxa() && purpleDoor === lockedDoor) {
+                        manterChaveIrParaBaixo();
+
+                    } else if (estaNaChaveRosa() && pinkDoor === lockedDoor) {
+                        manterChaveIrParaBaixo();
+
+                    } else if (sairDoBotao()) {
+                        manterBotaoIrParaBaixo();
+
+                    } else if (estaNumaPorta()) {
+                        manterPortaIrParaBaixo();
 
                     } else {
-                    fase02[coordPlayer.y][coordPlayer.x] = ' ';
-                    fase02[coordPlayer.y + 1][coordPlayer.x] = player;
-                    coordPlayer.y++;
-                    console.log(fase02);
-
+                        irParaBaixo();
                     }
                 }
                 break;
             case 65: // A
-                if (checarEspacoVazio(65) || checarEspacoChave(65) || checarPorta(65)) {
+                if (checarEspacoVazio(65) || checarEspacoChave(65) || checarPorta(65) || checarBotao(65)) {
 
-                    if (coordPlayer.x === coordKey.x && coordPlayer.y === coordKey.y && fase02[coordDoor.y][coordDoor.x] === lockedDoor) {
-                        fase02[coordPlayer.y][coordPlayer.x] = key;
-                        fase02[coordPlayer.y][coordPlayer.x - 1] = player;
-                        coordPlayer.x--;
-                        console.log(fase02);
+                    if (estaNaChaveVermelha() && redDoor === lockedDoor) {
+                        manterChaveIrParaEsquerda();
 
+                    } else if (estaNaChaveRoxa() && purpleDoor === lockedDoor) {
+                        manterChaveIrParaEsquerda();
+
+                    } else if (estaNaChaveRosa() && pinkDoor === lockedDoor) {
+                        manterChaveIrParaEsquerda();
+
+                    } else if (sairDoBotao()) {
+                        manterBotaoIrParaEsquerda();
+                    } else if (estaNumaPorta()) {
+                        manterBotaoIrParaEsquerda();
                     } else {
-                    fase02[coordPlayer.y][coordPlayer.x] = ' ';
-                    fase02[coordPlayer.y][coordPlayer.x - 1] = player;
-                    coordPlayer.x--;
-                    console.log(fase02);
-
+                        irParaEsquerda();
                     }
                 }
                 break;
             case 68: // D
-                if (checarEspacoVazio(68) || checarEspacoChave(68) || checarPorta(68)) {
+                if (checarEspacoVazio(68) || checarEspacoChave(68) || checarPorta(68) || checarBotao(68)) {
 
-                    if (coordPlayer.x === coordKey.x && coordPlayer.y === coordKey.y && fase02[coordDoor.y][coordDoor.x] === lockedDoor) {
-                        fase02[coordPlayer.y][coordPlayer.x] = key;
-                        fase02[coordPlayer.y][coordPlayer.x + 1] = player;
-                        coordPlayer.x++;
-                        console.log(fase02);
+                    if (estaNaChaveVermelha() && redDoor === lockedDoor) {
+                        manterChaveIrParaDireita();
 
+                    } else if (estaNaChaveRoxa() && purpleDoor === lockedDoor) {
+                        manterChaveIrParaDireita();
+
+                    } else if (estaNaChaveRosa() && pinkDoor === lockedDoor) {
+                        manterChaveIrParaDireita();
+
+                    } else if (sairDoBotao()) {
+                        manterBotaoIrParaDireita();
+                    } else if (estaNumaPorta()) {
+                        manterPortaIrParaDireita();
                     } else {
-                    fase02[coordPlayer.y][coordPlayer.x] = ' ';
-                    fase02[coordPlayer.y][coordPlayer.x + 1] = player;
-                    coordPlayer.x++;
-                    console.log(fase02);
-
+                        irParaDireita();
                     }
                 }
                 break;
             case 73: // I
-                if (estaNaChaveRosa() || estaNaChaveRoxa() || estaNaChaveVermelha()) {
-                    if(estaNaChaveVermelha()) {
-                        redDoor = unlockedDoor;
-                        fase02[fixedCoords.redDoor.y][fixedCoords.redDoor.x] = redDoor;
-                    }
-                    if(estaNaChaveRosa()) {
-                        pinkDoor = unlockedDoor;
-                        fase02[fixedCoords.pinkDoor.y][fixedCoords.pinkDoor.x] = pinkDoor;
-                    }
-                    if(estaNaChaveRoxa()) {
-                        purpleDoor = unlockedDoor;
-                        fase02[fixedCoords.pinkDoor.y][fixedCoords.pinkDoor.x] = purpleDoor;
+                if(estaNaChaveVermelha()) {
+                    redDoor = unlockedDoor;
+                    fase02[fixedCoords.redDoor.y][fixedCoords.redDoor.x] = redDoor;
+                    console.clear();
+                    console.table(fase02); 
+                }
+                if(estaNaChaveRosa()) {
+                    finalDoor = unlockedDoor;
+                    fase02[fixedCoords.finalDoor.y][fixedCoords.finalDoor.x] = finalDoor;
+                    console.clear();
+                    console.table(fase02); 
+                }
+                if(estaNaChaveRoxa()) {
+                    purpleDoor = unlockedDoor;
+                    fase02[fixedCoords.purpleDoor.y][fixedCoords.purpleDoor.x] = purpleDoor;
+                    console.clear();
+                    console.table(fase02); 
+                }                    
+                               
+                if(estaNoBotaoAmarelo()) {
+                    
+                }
+                if(estaNoBotaoAzulClaro()) {
+                    for (let j = 17; j < 29; j++) {
+                        if (j >= 18 && j < 20) {
+                            fase02[10][j] = spikes;
+                            if (j === 19) {
+                            fase02[11][j] = spikes;
+                            }
+                        } else if (j === 22) {
+                            fase02[13][j] = ' ';
+                            fase02[14][j] = ' ';
+                            fase02[15][j] = ' ';
+                        }
+                        if (j === 25) {
+                            for (let i = 23; i <= 27; i++) {
+                                fase02[i][25] = spikes;
+                            }
+                        } 
+                        if (j === 26) {
+                            fase02[23][j] = spikes;
+                            fase02[27][j] = spikes;
+                        }
+                        if (j === 27 || j === 28) {
+                            fase02[25][j] = spikes;
+                        }
                     }                    
-                    console.log(fase02)
+                    console.clear;
+                    console.table(fase02); 
                 }
-                if (estaNoBotaoAmarelo() || estaNoBotaoAzulClaro() || estaNoBotaoAzulEscuro() || estaNoBotaoLaranja() || estaNoBotaoVerde()) {
-                    if(estaNoBotaoAmarelo()) {
-                        
+                if(estaNoBotaoAzulEscuro()) {
+                    for (let i = 22; i <= 28; i++) {
+                        if (i === 22) {
+                            fase02[i][13] = spikes;
+                            fase02[i][19] = spikes;
+                        } else if (i === 23 || i === 24) {
+                            fase02[i][3] = spikes;
+                            fase02[i][13] = spikes;
+                        } else if (i === 25) {
+                            fase02[i][3] = spikes;
+                            fase02[i][13] = spikes;
+                            fase02[i][18] = spikes;
+                        } else if (i === 26 || i === 28) {
+                            fase02[i][9] = spikes;
+                            fase02[i][11] = ' ';
+                        } else if (i === 27) {
+                            fase02[i][9] = spikes;
+                            fase02[i][11] = spikes;
+                        }
                     }
-                    if(estaNoBotaoAzulClaro()) {
-                        
-                    }
-                    if(estaNoBotaoAzulEscuro()) {
-                        
-                    }   
-                    if(estaNoBotaoLaranja()) {
-                        
-                    }
-                    if(estaNoBotaoVerde()) {
-                        
-                    }                 
-                    console.log(fase02)
+                    console.clear();
+                    console.table(fase02);
+                }   
+                if(estaNoBotaoLaranja()) {
+                    
                 }
+                if(estaNoBotaoVerde()) {
+                    for (let j = 5; j <= 11; j++) {
+                        if (j <= 8) {
+                            fase02[7][j] = spikes;
+                        } else {
+                            fase02[7][j] = ' ';
+                            fase02[13][j] = spikes;
+                        }
+                    }
+                    for (let i = 10; i <= 13; i++) {
+                        fase02[i][8] = spikes;
+                    }
+                    console.clear;
+                    console.table(fase02); 
+                }    
+                
                 if (estaNaPortaFinal()) {
-                    console.log('proxima fase')
+                    alert('proxima fase')
                 }
     };
 });
